78587599c21612d910973cc589eec1918997249f
Muhammet Sefa DURMUŞ <35434462+sefa35@users.noreply.github.com>
2023-04-19 10:25:44 +0300

    Add files via upload

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Get, Post, Param, Delete } from '@nestjs/common';
+import { Task } from './tasks.model';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Delete('/:id')
+  deleteTaskById(@Param('id') id: string): void {
+    return this.tasksService.deleteTaskById(id);
+  }
+}
